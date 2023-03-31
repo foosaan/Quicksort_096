04932064804e6318597921fa069a8a0e0c696c99
foosaan <fauzan4297@gmail.com>
2023-03-31 15:24:30 +0700

    craete langakah 10 ,5,6,7,8,9

--- Quicksort_096/Quicksort_096.cpp
+++ Quicksort_096/Quicksort_096.cpp
@@ -48,8 +48,33 @@ void q_short(int low, int high)
 	//One containing elements less that or equal to pivot
 	//Outher conntainning elements greather than pivot 
 
-	pivot = arr[low];
-	//langkah 2
-	i = low + 1;
-	//langkah 3
-	j = high;
+	pivot = arr[low];//langkah 2
+	i = low + 1;//langkah 3
+	j = high;//langkah 4
+	
+	while (i <= j)//langkah 10
+	{
+		//Search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))	//langkah 5
+		{
+			i++;									//langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot 
+		while ((arr[j] > pivot) && (j >= low))		//langkah 7
+		{
+			j--;									//langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//Langkah 9
+		if (i < j) // if the greater element is on the left of the element
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	
